d06bfa23fb00caabdedd9a1abc1eed05933b0c3f
Mathieu <mathieurousseau123@gmail.com>
2021-07-08 21:34:32 -0700

    test insertMany method

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ mongoose
     console.log("OH NO THE CONNECTION DID NOT WORK!!");
     console.log(error);
   });
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+// to be able to make new instances of your movie class.
+// "Movie" must be singular and capitalized. Mongoose will lower case the word and pluralize it.
+// when you call an instance of a model like below, you need to hit save to inset in the db.
+const Movie = mongoose.model("Movie", movieSchema);
+// const amadeus = new Movie({
+//   title: "Amadeus",
+//   year: 1986,
+//   score: 9.2,
+//   rating: "R",
+// });
+
+// with this method, you don't need to hit save
+Movie.insertMany([
+  { title: "Amelie", year: 2001, score: 8.3, rating: "R" },
+  { title: "Alien", year: 1979, score: 8.1, rating: "R" },
+  { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
+  { title: "Stand by Me", year: 1986, score: 8.6, rating: "R" },
+  { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" },
+]).then((data) => {
+  console.log("IT WORKED, THE 5 PIECES OF DATA HAVE BEEN ADDED TO THE DB");
+  console.log(data);
+});
