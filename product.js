c5331c9d364aa23e0ac98b043750cc85a349e204
Mathieu <mathieurousseau123@gmail.com>
2021-07-09 11:32:23 -0700

    develop schema and first product

--- product.js
+++ product.js
@@ -11,3 +11,28 @@ mongoose
     console.log("OH NO THE CONNECTION DID NOT WORK!!");
     console.log(error);
   });
+
+// this method allows for validation from mongoose compared to the method used in index.js
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // this makes it so the name is required
+  },
+  price: {
+    type: Number,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+const bike = new Product({ name: "Mountain Bike", price: "599" });
+bike
+  .save()
+  .then((data) => {
+    console.log("IT WORKED!");
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log("OH NO ERROR!");
+    console.log(err.errors.name.properties.message);
+  });
