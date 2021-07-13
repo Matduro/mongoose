e91f2b6d586a8105b0f58fbeba634c64143de473
Mathieu <mathieurousseau123@gmail.com>
2021-07-13 09:36:51 -0700

    update mongoose path to relevant names

--- person.js
+++ person.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/test", {
+  .connect("mongodb://localhost:27017/people", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
--- product.js
+++ product.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/test", {
+  .connect("mongodb://localhost:27017/shopApp", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
